57c77b4fc8a31259c39168ae4a7f3ce8b9ee80d9
adit-bardiya <65112345+adit-bardiya@users.noreply.github.com>
2020-06-28 16:03:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var canvas,backgroundIMG;
+var gameState = 0;
+var playerCount,database,form,player,game;
+function setup(){
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+function draw(){}
